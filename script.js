04b546be4d77701726b79440916848339f366383
karinapichardo <pichardokarina21@yahoo.com>
2021-04-25 21:08:07 -0400

    added functionality to dots

---
+++ script.js
@@ -0,0 +1,41 @@
+function firstDot() {
+    document.getElementById('project1').style.display = 'block';
+    document.getElementById('project2').style.display = 'none';
+    document.getElementById('project3').style.display = 'none';
+    document.getElementById('project4').style.display = 'none';
+    document.getElementById('project5').style.display = 'none';
+}
+
+function secondDot() {
+    document.getElementById('project2').style.display = 'block';
+    document.getElementById('project1').style.display = 'none';
+    document.getElementById('project3').style.display = 'none';
+    document.getElementById('project4').style.display = 'none';
+    document.getElementById('project5').style.display = 'none';
+}
+
+function thirdDot() {
+    document.getElementById('project3').style.display = 'block';
+    document.getElementById('project1').style.display = 'none';
+    document.getElementById('project2').style.display = 'none';
+    document.getElementById('project4').style.display = 'none';
+    document.getElementById('project5').style.display = 'none';
+}
+
+function fourthDot() {
+    document.getElementById('project4').style.display = 'block';
+    document.getElementById('project1').style.display = 'none';
+    document.getElementById('project2').style.display = 'none';
+    document.getElementById('project3').style.display = 'none';
+    document.getElementById('project5').style.display = 'none';
+
+}
+
+function fifthDot() {
+    document.getElementById('project5').style.display = 'block';
+    document.getElementById('project1').style.display = 'none';
+    document.getElementById('project2').style.display = 'none';
+    document.getElementById('project3').style.display = 'none';
+    document.getElementById('project4').style.display = 'none';
+
+}
