0895e58406f2cce1df61fdb8afd2517ef94e510e
karinapichardo <pichardokarina21@yahoo.com>
2021-04-26 22:01:48 -0400

    added text change function for intro title

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+var text = ['Karina Pichardo.', 'a Frontend Developer.']
+var index = 0;
+var element = document.getElementById('name-title');
+
+window.setInterval(function textChange() {
+    element.innerHTML = text[index];
+    index++;
+
+    if (index >= text.length) {
+        index = 0;
+    }
+}, 1500);
+
 function firstDot() {
     document.getElementById('project1').style.display = 'block';
     document.getElementById('project2').style.display = 'none';
