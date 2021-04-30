7eb62b6cceb29240935858f33bc5127e641c4326
karinapichardo <pichardokarina21@yahoo.com>
2021-04-29 21:15:40 -0400

    added functionality to color mode button

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ function hamburgerMenu() {
     }
 }
 
+function colorMode() {
+    document.body.style.background = 'black';
+    document.body.style.color = 'white';
+    document.querySelector('.cert-link').style.color = '#ffc200';
+
+    var j, k;
+    j = document.querySelectorAll('.mode-text');
+
+    for (k = 0; k < j.length; k++) {
+        j[k].style.color = 'white';
+    }
+
+    var x, i;
+    x = document.querySelectorAll('.mode-color');
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.backgroundColor = '#ff94b4';
+        x[i].style.opacity = '1';
+    }
+}
 var text = ['Karina Pichardo.', 'a Frontend Developer.']
 var index = 0;
 var element = document.getElementById('name-title');
